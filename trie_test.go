59a13afd60c3b7cbc7b4e40a35d1b3e9eeee8cbf
zhj <abc1143356987@163.com>
2020-03-23 17:52:59 +0800

    test文件

--- trie_test.go
+++ trie_test.go
@@ -7,6 +7,7 @@ import (
 
 func TestTrie(t *testing.T) {
 	obj := NewTrie()
+	//(1)前缀搜索,智能提示
 	//第一个参数为要加入Trie中的字符串,第二个参数为字符串叶子节点保存的数据
 	obj.Add("相框", "相框")
 	obj.Add("相框摆", "相框摆")
@@ -14,13 +15,12 @@ func TestTrie(t *testing.T) {
 	obj.Add("相框挂", "相框挂")
 	obj.Add("相框挂台", "相框挂台")
 
-	//前缀搜索,自动推荐
 	data := obj.PrefixSearch("相框", 3)
 	for _, v := range data {
 		fmt.Println(v)
 	}
 
-	//查看当前字符串是否在trie树中
+	//(2)查看当前字符串是否在trie树中
 	mgc := obj.Search("相")
 	fmt.Println(mgc)
 
@@ -31,13 +31,13 @@ func TestTrie(t *testing.T) {
 	//ac.AddPattern("真")
 	//ac.Build()
 
-	//敏感词替换
+	//(3)敏感词替换
 	trie := NewTrie()
 	//将敏感词加入到Trie中,
 	trie.Add("tm", "tm")
+	trie.Add("电影","电影")
 	trie.Add("他妈", nil)
 
 	result, str := trie.Replace("这个电影真tm的是难看,好他妈难看啊", "*")
-
 	fmt.Printf("result:%#v, str:%v\n", result, str)
 }
